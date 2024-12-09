7ca6c2eb50700f21cc53b616b9df52e8d75c959f
Mrs. Adams <colleen_m_adams@mcpsmd.org>
2024-12-09 14:46:12 +0000

    {your message}

---
+++ StackQueueMini.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
+
+class StackQueueMini {
+  public static void main(String[] args) {
+
+  } // end main
+} // end class
